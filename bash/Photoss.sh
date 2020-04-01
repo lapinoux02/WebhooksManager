688cf450805337fa293e2ef2fa01c77169a0a190
lapinoux02 <paulbnt@free.fr>
2020-04-01 20:58:15 +0200

    Ajout de la sécurité des hooks. Ajout de l'execution de bash en fonction du nom du repo modifié.

---
+++ bash/Photoss.sh
@@ -0,0 +1,2 @@
+cd ../Photoss
+git pull
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var crypto = require('crypto');
+var execSync = require('child_process').execSync;
+var app = express();
+var port = 8002;
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Hub-Signature");
+	next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+function verifyPostData(req, res, next) {
+	const payload = JSON.stringify(req.body);
+	if (!payload) {
+		return next('Request body empty');
+	}
+
+	const sig = req.get('X-Hub-Signature') || '';
+	const hmac = crypto.createHmac('sha1', process.env.WEBHOOK_SECURE_PHRASE);
+	const digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8');
+	const checksum = Buffer.from(sig, 'utf8');
+	console.log(digest.toString('utf8'));
+	console.log(sig);
+	if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
+		return next(`Request body digest did not match X-Hub-Signature.`);
+	}
+	return next();
+}
+
+app.post('/', verifyPostData, (req, res) => {
+	execSync(`sh bash/${req.body.repository.name}.sh`);
+});
+
+app.listen(port);
+console.log('Webhooks manager started on: ' + port);
