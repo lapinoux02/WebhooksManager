3550a43fa96097166c4a9cf56b9525e10df7a113
lapinoux02 <paulbnt@free.fr>
2020-04-23 19:02:21 +0200

    Fix MtgCounter

--- scripts/WebhooksManager.js
+++ scripts/WebhooksManager.js
@@ -1,5 +1,6 @@
 module.exports = webhooksManagerScript;
 
 function webhooksManagerScript () {
+	const execSync = require('child_process').execSync;
 	require('child_process').execSync(`sh bash/WebhooksManager.sh`);
 }
