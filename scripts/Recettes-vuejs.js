73db15dacba6448a7f35de48851a886be8d7ce57
marjo1992 <verelmarjolaine@hotmail.fr>
2021-04-03 14:01:54 +0200

    Add force update on recettesvuejs

--- scripts/Recettes-vuejs.js
+++ scripts/Recettes-vuejs.js
@@ -3,5 +3,5 @@ module.exports = recettesVueJs;
 function recettesVueJs() {
 	const folder = '../Recettes-vuejs';
 	
-	require('child_process').execSync(`cd ${folder} && git pull`);
+	require('child_process').execSync(`cd ${folder} && git fetch && git reset --hard origin/master`);
 }
