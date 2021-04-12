295a2a6ec7679c5e09660582948a751fedfed80e
lapinoux02 <paulbnt@free.fr>
2021-04-12 18:44:06 +0200

    Changed the IFI master branch to main

--- scripts/IFI.js
+++ scripts/IFI.js
@@ -3,5 +3,5 @@ module.exports = ifiScript;
 function ifiScript () {
 	const folder = '../IFI';
 	
-	require('child_process').execSync(`cd ${folder} && git fetch && git reset --hard origin/master`);
+	require('child_process').execSync(`cd ${folder} && git fetch && git reset --hard origin/main`);
 }
